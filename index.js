9a7c4393445d6a9d202235649b6a584a1b9235d3
mari0994 <mbabiy@mail.ru>
2023-10-20 15:15:02 +0300

    update js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const input = document.querySelector('#task');
 const list = document.querySelector('.task-list');
 
 button.addEventListener('click', function(){
-    const listItem = document.createElement('li');
-    listItem.textContent = input.value;
-    list.append(listItem);
-    input.value = '';
+    if (input.value !== ''){
+        const listItem = document.createElement('li');
+        listItem.textContent = input.value;
+        list.append(listItem);
+        input.value = '';
+    }
 })
 
 list.addEventListener('click', function(event){
